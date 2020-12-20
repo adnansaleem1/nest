8d6d3e87af936259a58f728619e8f95a0c1c0117
adnansaleemse <adnansaaleemse@gmail.com>
2020-12-20 23:03:44 +0500

    orm with graphql

--- sample/05-sql-typeorm/src/app.module.ts
+++ sample/05-sql-typeorm/src/app.module.ts
@@ -1,20 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { CatsModule } from './cats/cats.module';
+
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres',
       host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
+      port: 5432,
+      username: 'postgres',
+      password: 'sasa',
       database: 'test',
       autoLoadEntities: true,
       synchronize: true,
     }),
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      installSubscriptionHandlers: true,
+    }),
     UsersModule,
+    CatsModule,
   ],
 })
 export class AppModule {}
